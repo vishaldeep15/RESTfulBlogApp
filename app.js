933e577693d903939cebc00daa347f107afb4ae6
Vishal Deep <visdhiman15@gmail.com>
2018-01-12 14:03:35 -0500

    added delete route and final UI improvements

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var express     = require("express"),
-	app         = express(),
-	bodyParser  = require("body-parser"),
-	mongoose    = require("mongoose"),
-	methodOverride = require("method-override");
+var express        = require("express"),
+	app            = express(),
+	bodyParser     = require("body-parser"),
+	mongoose       = require("mongoose"),
+	methodOverride = require("method-override"),
+	expressSanitizer = require("express-sanitizer");
 
 //App config
 mongoose.connect("mongodb://localhost/restful_blog_app", {useMongoClient: true});
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //Schema Setup
 var blogSchema = new mongoose.Schema({
@@ -45,6 +47,7 @@ app.get("/blogs/new", function(req, res){
 
 //Create Route
 app.post("/blogs", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){
 			console.log(err);
@@ -78,11 +81,23 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // Update Route
 app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
 		if(err) {
 			res.redirect("/blogs");
 		} else {
 			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+
+//Delete Route
+app.delete("/blogs/:id", function(req, res) {
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
 		}
 	});
 });
